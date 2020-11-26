cf13f996958bb7321577d15b521cd003cc65e50f
Abolfazl Ghasemihesar <soozanfoolad9@gmail.com>
2020-11-26 15:25:31 +0330

    render home.html and about.html in views.py

--- weblog/weblog/views.py
+++ weblog/weblog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 def home(request):
-    return HttpResponse('Welcome to our Weblog!');
-
+    # return HttpResponse('Welcome to our Weblog!');
+    return render(request,'Home.html')
 def about(request):
-    return HttpResponse('About is a Hello World!');
+    # return HttpResponse('About is a Hello World!');
+    return render(request,'about.html')
