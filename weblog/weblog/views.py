828f26d77418c6c1c2fa3ac8aad170478fe98fff
Abolfazl Ghasemihesar <soozanfoolad9@gmail.com>
2020-11-26 15:03:36 +0330

    Create home function and show welcome to users

--- weblog/weblog/views.py
+++ weblog/weblog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
+def home(request):
+    return HttpResponse('Welcome to our Weblog!');
+
 def about(request):
     return HttpResponse('About is a Hello World!');
